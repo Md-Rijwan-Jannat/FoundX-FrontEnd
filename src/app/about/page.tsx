5dfd33d523a2b270ec9920a3a0ca309bea260c14
Md-Rijwan-Jannat <rijwanjannat36@gmail.com>
2024-09-10 21:50:11 +0600

    create a found nextjs app and sonfigure nextui

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+type TAboutPageProps = object;
+
+const AboutPage: FC<TAboutPageProps> = () => {
+  return (
+    <div>
+      <h2>This is a AboutPage component</h2>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+type THomePageProps = object;
+
+const HomePage: FC<THomePageProps> = () => {
+  return (
+    <div>
+      <h2>This is a HomePage component</h2>
+    </div>
+  );
+};
+
+export default HomePage;
